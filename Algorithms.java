7951ed5eddde5928e193bc7e6700230d7bd623db
Thanh0721 <thanhn2789@nycstudents.net>
2024-12-04 09:29:08 -0500

    Update Algorithms.java

--- Algorithms.java
+++ Algorithms.java
@@ -5,21 +5,37 @@ import java.io.FileNotFoundException;
 public class Algorithms {
     private static File f;
     private static Scanner s;
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException 
+    {
         f = new File("Numbers.txt");
         int odds = odds();
         int evens = evens();
-        System.out.println(odds);
+        System.out.println("Odd: " + odds);
+        System.out.println("Even: " + evens);
         s.close();
     }
 
-    public static int odds() throws FileNotFoundException{
+    public static int odds() throws FileNotFoundException
+    {
         s = new Scanner(f);
         int odds = 0;
-        while (s.hasNext()) {
+        while (s.hasNext()) 
+        {
             if (s.nextInt() % 2 != 0)
                 odds++;
         }
         return odds;
+    }
+
+    public static int evens() throws FileNotFoundException
+    {
+        s = new Scanner(f);
+        int evens = 0;
+        while (s.hasNext())
+        {
+            if (s.nextInt() % 2 == 0)
+                evens++;
+        }
+        return evens;
     }
 }
